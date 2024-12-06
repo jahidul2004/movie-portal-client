b771b4f49bdc9e875ec7f3d7a7177aa77b8a3ac0
jahidul2004 <islamjahiduljihad@gmail.com>
2024-12-06 17:20:48 +0600

    Footer section added.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,35 @@
+import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
+import { RiMovie2AiLine } from "react-icons/ri";
+
+const Footer = () => {
+    return (
+        <footer className="footer footer-center bg-[#f7f7f7] p-10">
+            <aside>
+                <RiMovie2AiLine className="text-4xl font-bold text-[#e50912]" />
+                <p className="font-bold">
+                    Movie Portal
+                    <br />
+                    Providing best movies since 1992
+                </p>
+                <p>
+                    Copyright © {new Date().getFullYear()} - All right reserved
+                </p>
+            </aside>
+            <nav>
+                <div className="grid text-2xl grid-flow-col gap-4">
+                    <a>
+                        <FaFacebook />
+                    </a>
+                    <a>
+                        <FaInstagram />
+                    </a>
+                    <a>
+                        <FaYoutube />
+                    </a>
+                </div>
+            </nav>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/mainlayout/MainLayout.jsx
+++ src/mainlayout/MainLayout.jsx
@@ -1,10 +1,18 @@
+import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
     return (
         <div>
             {/* Nav Bar */}
             <NavBar></NavBar>
+
+            {/* Outlet */}
+            <Outlet></Outlet>
+
+            {/* Footer */}
+            <Footer></Footer>
         </div>
     );
 };
