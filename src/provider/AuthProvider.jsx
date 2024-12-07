51dc540f27ebe688b90ee6641060649dde77b8cd
jahidul2004 <islamjahiduljihad@gmail.com>
2024-12-07 12:19:29 +0600

    On Auth state changed functionality added.

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,22 +1,44 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { auth } from "../firebase/firebase.init";
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import {
+    createUserWithEmailAndPassword,
+    onAuthStateChanged,
+    updateProfile,
+} from "firebase/auth";
 
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
 
     const createUser = (email, password) => {
-        return createUserWithEmailAndPassword(auth, email, password)
-    }
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
 
     const updateUser = (user) => {
         return updateProfile(auth.currentUser, user);
-    }
+    };
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            if (currentUser) {
+                setUser(currentUser);
+            } else {
+                console.log("User is logged out");
+                setUser(null);
+            }
+        });
+
+        return () => {
+            unsubscribe();
+        };
+    }, []);
 
     const authInfo = {
         createUser,
         updateUser,
+        user,
+        setUser,
     };
 
     return (
