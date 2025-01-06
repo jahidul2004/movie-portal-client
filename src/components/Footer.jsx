7b9fe80d253710631a12d594813b019b5b9af260
jahidul2004 <islamjahiduljihad@gmail.com>
2025-01-06 21:24:48 +0600

    Footer updated.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
 import { RiMovie2AiLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
-        <footer className="footer footer-center p-10">
+        <footer className="footer gap-3 footer-center p-10">
             <aside>
                 <RiMovie2AiLine className="text-6xl font-bold text-[#e50912]" />
                 <p className="text-xl font-bold">
@@ -16,7 +17,22 @@ const Footer = () => {
                 </p>
             </aside>
             <nav>
-                <div className="grid text-2xl grid-flow-col gap-4">
+                <ul className="flex gap-3">
+                    <li>
+                        <Link to={"/"}>Home</Link>
+                    </li>
+                    <li>
+                        <Link to={"/trendingNow"}>Trending Now</Link>
+                    </li>
+                    <li>
+                        <Link to={"/favoriteMovies"} href="#">
+                            Favorites
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+            <nav>
+                <div className="grid text-[#e50912] text-2xl grid-flow-col gap-4">
                     <a>
                         <FaFacebook />
                     </a>
